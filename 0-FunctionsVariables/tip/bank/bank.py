332a8107608b1f9e227ac9b23096a1a51faaf6e1
Clifford-La <keishi.la@education.nsw.gov.au>
2025-12-15 23:17:55 +0000

    working on it

---
+++ 0-FunctionsVariables/tip/bank/bank.py
@@ -0,0 +1,12 @@
+greeting = input("Greetings: ")
+
+
+if greeting == ("hello"):
+    print("$0")
+elif greeting == gt():
+    for letter in greeting:
+        letter[0] = "h"
+        print("$20")
+
+else:
+    print("$100")
