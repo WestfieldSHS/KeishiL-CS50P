0f055bf1b3c252726328fe5657481103d311f56a
Clifford-La <keishi.la@education.nsw.gov.au>
2026-02-09 01:52:04 +0000

    commit

--- 0-FunctionsVariables/tip/deep.py
+++
@@ -1,9 +0,0 @@
-msg = input("Great Question of Life? ")
-if msg == ("forty-two"):
-    print("Yes")
-elif msg == ("forty two"):
-    print("Yes")
-elif msg == ("42"):
-    print("Yes")
-else:
-    print("No")
