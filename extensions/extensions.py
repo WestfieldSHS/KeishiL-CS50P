bf1fa2c42c17a82d7868a8932f28b565c04ca164
Clifford-La <keishi.la@education.nsw.gov.au>
2026-02-16 22:13:14 +0000

    commit

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,12 +1,23 @@
-file = input("Attach your file: ")
-list = [".png", ".jpeg", ".jpg"]
-list2 = [".pdf"]
-list3 = [".txt"]
-if file in list:
-    print("This file is an image")
-elif file in list2:
-    print("This is a PDF")
-elif file in list3:
-    print("This file is a text")
-else:
-    print("")
+def main():
+    file = input("Attach your file: ")
+    file = file.lower()
+    file = file.replace(" ","")
+
+    if file .endswith(".png"):
+        print("Image/png")
+    elif file .endswith(".jpg") or file .endswith(".jpeg"):
+        print("Image/jpg")
+    elif file . endswith(".pdf"):
+        print("application/pdf")
+    elif file .endswith(".gif"):
+        print("Image/gif")
+    elif file .endswith(".txt"):
+        print("text/text")
+    elif file .endswith(".pdf"):
+        print("document/pdf")
+    elif file .endswith(".zip"):
+        print("file/zip")
+    else:
+        print("application/octet-stream")
+
+main()
