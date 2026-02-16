b49a38d1e21e6a36368b2e1a10b95c72ce5738b0
Clifford-La <keishi.la@education.nsw.gov.au>
2026-02-16 22:38:37 +0000

    commit math

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,21 @@
+expression = input("Equation: ")
+x, y, z = expression.split(" ")
+x = float(x)
+z = float(z)
+
+if y == "+":
+    answer = x + z
+    print(answer)
+elif y == "-":
+    answer = x - z
+    print(answer)
+elif y == "*":
+    answer = x * z
+    print(answer)
+elif y == "/" and z == 0:
+    print("Error") 
+elif y == "/":
+    answer = x / z
+    print(answer)
+else:
+    print("########################")
